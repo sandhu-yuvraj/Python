b995788f273ec8354c406eb210b1af3fa810cd49
Yuvraj Singh Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-22 11:52:59 +0530

    Change input type from int to float for dimensions

--- area-of-rectangle.py
+++ area-of-rectangle.py
@@ -1,4 +1,4 @@
-length = int(input("Enter the length of the rectangle: "))
-breadth = int(input("Enter the width of the rectangle: "))
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the width of the rectangle: "))
 area = length * breadth
 print(area)
