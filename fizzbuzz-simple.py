3d7bc10385f2e456be2e279385fb7f7112d21be4
Yuvraj Singh Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-27 12:25:49 +0530

    Add comments to explain FizzBuzz logic
    Added comments explaining the FizzBuzz problem and its requirements.

--- fizzbuzz-simple.py
+++ fizzbuzz-simple.py
@@ -1,3 +1,8 @@
+# The FizzBuzz problem is a classic programming interview challenge. 
+# It requires writing a program that iterates through numbers up to a given limit. 
+# For multiples of 3, it prints "Fizz",
+# for multiples of 5, it prints "Buzz", 
+# and for multiples of both, it prints "FizzBuzz"
 number = int(input("Enter a number: "))
 if (number%3 == 0) and (number%5 == 0):
     print("Divisible by both 3 and 5 , FizzBuzz")
