9d7bbe9d3eed6de1f7d3289ef21a46be06887d66
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-27 17:09:11 +0530

    Typo

--- tip.py
+++ tip.py
@@ -1,4 +1,4 @@
-amount = float(input("Enter the amount in dollars: "))
+amount = float(input("Enter the amount of bill in dollars: "))
 tip_percentage = float(input("Enter the tip percentage: ")) #write without the % sign
 tip = amount * tip_percentage / 100
 print("Tip amount: $", tip)
