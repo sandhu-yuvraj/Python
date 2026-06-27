adb8c9cd20b8184c048af8d2fc0a439fb4efd2de
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-27 12:18:59 +0530

    Modified

--- fizzbuzz-simple.py
+++ fizzbuzz-simple.py
@@ -1,9 +1,9 @@
 number = int(input("Enter a number: "))
 if (number%3 == 0) and (number%5 == 0):
-    print("Divisible by both 3 and 5")
+    print("Divisible by both 3 and 5 , FizzBuzz")
 elif (number%3 == 0):
-    print("Divisible by 3")
+    print("Divisible by 3 , Fizz")
 elif (number%5 == 0):
-    print("Divisible by 5") 
+    print("Divisible by 5 , Buzz") 
 else:
     print("Not divisible by 3 or 5")
